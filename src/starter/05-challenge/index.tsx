c73a65c812df503f7c8a18cec887ede59272e547
d-libards <dominiquelibardo09@gmail.com>
2024-11-21 03:38:36 +0800

    challenge

--- src/starter/05-challenge/index.tsx
+++ src/starter/05-challenge/index.tsx
@@ -1,9 +1,30 @@
-function Component() {
+type BasicProps = {
+  type: 'basic';
+  name: string;
+};
+
+type AdvancedProps = {
+  type: 'advanced';
+  name: string;
+  email: string;
+};
+
+type Props = AdvancedProps | BasicProps;
+
+function Component(props: Props) {
+  const { type, name } = props;
+  if (type === 'advanced') {
+    return (
+      <article className="alert alert-danger">
+        <h2>user: {name}</h2>
+        <h2>email: {props.email}</h2>
+      </article>
+    );
+  }
   return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Challenge</h2>
-    </div>
+    <article className="alert alert-success">
+      <h2>user: {name}</h2>
+    </article>
   );
 }
 export default Component;
