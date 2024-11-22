e2062696c5a309e72f120e12ba2fba91a0ac474a
d-libardsv2 <dominiquelibardo09@gmail.com>
2024-11-23 03:27:29 +0800

    redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-
+import { store } from './store.ts';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
@@ -10,6 +9,8 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </QueryClientProvider>
 );
